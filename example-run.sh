749f5d1cab0928c52b1d9ed3557c0b5055afce60
Peter Hofmann <scm@uninformativ.de>
2016-01-31 14:05:44 +0100

    Add manpage, trim example-run.sh

--- example-run.sh
+++ example-run.sh
@@ -2,8 +2,8 @@
 
 while sleep 1
 do
-    echo 0                                # Monitor 0, see note below
-    echo 0"$(date +%s)"                   # Select style 0, some text
+    echo 0                                # Monitor 0
+    echo 0"$(date +%s)"                   # Select style 0
     echo 1I am pressed                    # Select style 1
     echo 2This is another style
     echo -                                # Request an empty segment
@@ -19,9 +19,6 @@ do
     echo e
 
     echo f
-
-    # Note: Instead of a monitor index, you can also use "a" to show the
-    # same text on all monitors.
 done | ./bevelbar \
     left bottom \
     10 5 \
@@ -33,32 +30,3 @@ done | ./bevelbar \
     '#bebebe' '#000000' '#e1e1e1' '#747474' \
     '#bebebe' '#000000' '#747474' '#e1e1e1' \
     '#a11212' '#ffffff' '#e61919' '#961111'
-
-# Arguments:
-#
-# "left", "bottom"      Horizontal and vertical position (one of "left",
-#                       "center", "right" and "top", "bottom")
-#
-# "10", "5"             Horizontal and vertical margin (distance to screen edge)
-#
-# "3", "2"              Size of outer and inner bevel
-#
-# "0.25", "0.5"         Factors to define the size of space between segments
-#                       and of empty segments. Will be multiplied by font
-#                       height.
-#
-# "Terminus..."         Font, obviously.
-#
-# "#a3a3a3"             Color of empty segments.
-#
-# "#b1b1b1", "#363636"  Bright and dark color of outer bevel border.
-#
-# group of 4 colors     These are the user defined styles which you can
-#                       select in your input data (see above). First
-#                       color is background color, second color is text
-#                       color, third color is this segment's bright
-#                       bevel color, fourth color is the dark bevel
-#                       color.
-#
-# Please note: *ALL* of these arguments and at least one style
-# description have to be given.
